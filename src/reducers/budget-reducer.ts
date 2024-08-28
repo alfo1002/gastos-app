e2b727ad2e4d2e632cf6ca83ff64a2b8fbd33e35
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-08-27 21:44:49 -0500

    add: se agrego funcionalidad de deslizar gasto a la izquierda para eliminar

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -5,7 +5,8 @@ export type BudgetActions =
     { type: 'add-budget', payload: { budget: number } } |
     { type: 'show-modal' } |
     { type: 'close-modal' } |
-    { type: 'add-expense', payload: { expense: DraftExpense } }
+    { type: 'add-expense', payload: { expense: DraftExpense } } |
+    { type: 'remove-expense', payload: { id: Expense['id'] } }
 
 export type BudgetState = {
     budget: number
@@ -56,6 +57,13 @@ export const budgetReducer = (
             modal: false
         }
     }
+    if (action.type === 'remove-expense') {
+        return {
+            ...state,
+            expenses: state.expenses.filter(expense => expense.id !== action.payload.id)
+        }
+    }
+
 
     return state
 }
